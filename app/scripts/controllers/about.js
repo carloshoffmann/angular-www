c262081109725cf5966c900d1ac657063e0487a4
Carlos Hoffmann <choffmanne@gmail.com>
2017-03-11 09:12:51 -0300

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hoffmannWebhostingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hoffmannWebhostingApp
+ */
+angular.module('hoffmannWebhostingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
